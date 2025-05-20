76970ba2b717abac618baf8c1f8cb2848f59be7f
Henri <84406393+hky31@users.noreply.github.com>
2025-05-20 21:40:22 +0200

    Delete Entities directory -- miscommit Delete Persistence directory -- miscommit Delete Services directory -- miscommit Delete Simulator directory -- miscommit Delete ForestController.cs -- miscommit Delete Program-old.cs -- miscommit Delete Program.cs -- miscommit Delete forest.component.html -- miscommit Delete forest.component.ts -- miscommit Delete forest.service.ts -- miscommit Delete forest.ts -- miscommit

--- forest.ts
+++
@@ -1,7 +0,0 @@
-export interface Forest {
-  size: number;
-  cells: TreeState[][]; // tableau jagged
-}
-
-// 0 | 1 | 2 | 3 = 'Empty' | 'Tree' | 'Fire' | 'Ash';
-export type TreeState = 0 | 1 | 2 | 3;
